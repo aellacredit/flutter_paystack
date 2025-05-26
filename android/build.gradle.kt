e1fc8094bbad5bc1f90cd3d548b350f93a9caf13
Alexander Nitiola <anitiola@aellacredit.com>
2025-05-26 07:44:28 +0100

    removed unused dependencies

--- android/build.gradle.kt
+++ android/build.gradle.kt
@@ -7,20 +7,27 @@ group = "co.paystack.flutterpaystack"
 version = "1.0-SNAPSHOT"
 
 android {
-    compileSdk = 32
+    namespace = "co.paystack.flutterpaystack"
+    compileSdk = 34
 
-    sourceSets["main"].java.srcDirs("src/main/kotlin")
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/kotlin")
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     defaultConfig {
         minSdk = 16
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionCode = 21
-        versionName = "3.0.10"
     }
 
     lint {
@@ -34,5 +41,13 @@ repositories {
 }
 
 dependencies {
-    implementation("com.google.android.material:material:1.7.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.22")
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("com.google.android.material:material:1.9.0")
+
+    // Flutter dependencies
+    compileOnly("io.flutter:flutter_embedding_debug:1.0.0-0fdb562ac8069a8b4e83b69a2c3b7e5daaa9aa2b")
+
+    // Paystack Android SDK
+    implementation("co.paystack.android:paystack:3.1.5")
 }
